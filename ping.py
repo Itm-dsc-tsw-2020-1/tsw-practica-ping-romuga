3f517bf25c752464ee1173a71ba41982322f8f9d
Ross Muñoz <55468439+romuga@users.noreply.github.com>
2020-02-27 12:34:09 -0600

    Delete ping.py

--- ping.py
+++
@@ -1,8 +0,0 @@
-import os
-hostname = "www.itmorelia.edu.mx"
-respuesta = os.system("ping -c 1 " + hostname)
-
-if respuesta == 0:
-    print (hostname + ": está en funcionamiento!")
-else:
-    print (hostname + ": No funciona!")

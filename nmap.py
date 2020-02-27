8dcba70d8cb11323cdccbef424bed2ae4261005e
romuga <rossmg1996@gmail.com>
2020-02-27 12:23:20 -0600

    Prueba con nmap

--- nmap.py
+++ nmap.py
@@ -1,3 +1,29 @@
 import os
 red = "200.33.171.0/24"
 os.system("nmap -sP " + red)
+
+
+"""
+Starting Nmap 7.60 ( https://nmap.org ) at 2020-02-27 12:15 CST
+Nmap scan report for mail.itmorelia.edu.mx (200.33.171.3)
+Host is up (0.029s latency).
+Nmap scan report for 200.33.171.13
+Host is up (0.12s latency).
+Nmap scan report for dsc.itmorelia.edu.mx (200.33.171.20)
+Host is up (0.043s latency).
+Nmap scan report for 200.33.171.63
+Host is up (0.0024s latency).
+Nmap scan report for 200.33.171.66
+Host is up (0.0023s latency).
+Nmap scan report for denebvirtual.itmorelia.edu.mx (200.33.171.77)
+Host is up (0.028s latency).
+Nmap scan report for 200.33.171.85
+Host is up (0.053s latency).
+Nmap scan report for 200.33.171.86
+Host is up (0.060s latency).
+Nmap scan report for 200.33.171.99
+Host is up (0.11s latency).
+Nmap scan report for 200.33.171.115
+Host is up (0.023s latency).
+Nmap done: 256 IP addresses (10 hosts up) scanned in 9.13 seconds
+"""
